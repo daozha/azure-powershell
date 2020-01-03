80996525cc51fdf1d756fd773c8d30da209bbd2e
Daotian Zhang <daozha@microsoft.com>
2020-01-03 13:44:58 +0800

    simplify test case to make it runnable locally

--- src/StackAdmin/Azs.Backup.Admin/test/Set-AzsBackupConfiguration.Tests.ps1
+++ src/StackAdmin/Azs.Backup.Admin/test/Set-AzsBackupConfiguration.Tests.ps1
@@ -24,17 +24,11 @@ Describe 'Set-AzsBackupConfiguration' {
         try
         {
             [System.IO.File]::WriteAllBytes($global:encryptionCertPath, [System.Convert]::FromBase64String($global:encryptionCertBase64))
-            $location = Set-AzsBackupConfiguration -Username $global:username -Password $global:password -Path $global:path -EncryptionCertPath $global:encryptionCertPath -IsBackupSchedulerEnabled:$global:isBackupSchedulerEnabled -BackupFrequencyInHours $global:backupFrequencyInHours -BackupRetentionPeriodInDays $global:backupRetentionPeriodInDays
+            $location = Set-AzsBackupConfiguration -IsBackupSchedulerEnabled:$global:isBackupSchedulerEnabled
             ValidateBackupLocation -BackupLocation $location
 
             $location                             | Should Not Be $Null
-            $location.Path                        | Should Be $global:path
-            $location.Username                    | Should be $global:username
-            $location.Password                    | Should -BeNullOrEmpty
-            $location.EncryptionCertBase64        | Should -BeNullOrEmpty
             $location.IsBackupSchedulerEnabled    | Should be $global:isBackupSchedulerEnabled
-            $location.BackupFrequencyInHours      | Should be $global:backupFrequencyInHours
-            $location.BackupRetentionPeriodInDays | Should be $global:backupRetentionPeriodInDays
         }
         finally
         {
@@ -52,24 +46,12 @@ Describe 'Set-AzsBackupConfiguration' {
         {
             [System.IO.File]::WriteAllBytes($global:encryptionCertPath, [System.Convert]::FromBase64String($global:encryptionCertBase64))
             $location = Get-AzsBackupConfiguration
-            $location.UserName = $global:username
-            $location.Password = $global:password
-            $location.Path = $global:path
-            $location.EncryptionCertBase64 = $global:encryptionCertBase64
             $location.IsBackupSchedulerEnabled = $global:isBackupSchedulerEnabled
-            $location.BackupFrequencyInHours = $global:backupFrequencyInHours
-            $location.BackupRetentionPeriodInDays = $global:backupRetentionPeriodInDays
             $result = $location | Set-AzsBackupConfiguration
             ValidateBackupLocation -BackupLocation $result
 
             $result                             | Should Not Be $Null
-            $result.Path                        | Should Be $global:path
-            $result.Username                    | Should be $global:username
-            $result.Password                    | Should -BeNullOrEmpty
-            $result.EncryptionCertBase64        | Should -BeNullOrEmpty
             $result.IsBackupSchedulerEnabled    | Should be $global:isBackupSchedulerEnabled
-            $result.BackupFrequencyInHours      | Should be $global:backupFrequencyInHours
-            $result.BackupRetentionPeriodInDays | Should be $global:backupRetentionPeriodInDays
         }
         finally
         {
